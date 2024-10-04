fa042e94a97d9595bfeea511441f8a884bc48070
Ian McAllister <iemcallister@gmail.com>
2024-10-04 13:02:28 -0700

    Adding tree array

---
+++ challenge19/treeArray.py
@@ -0,0 +1,21 @@
+# Challenge 19
+# Create a tree and store in an array
+
+# Init the tree
+tree = []
+
+# Function to insert a node in the tree
+def insert_node(value):
+    # Append the value of the tree list
+    tree.append(value)
+
+# Insert nodes
+insert_node(5)
+insert_node(3)
+insert_node(8)
+insert_node(2)
+insert_node(4)
+insert_node(7)
+
+# Print the tree
+print(tree)
